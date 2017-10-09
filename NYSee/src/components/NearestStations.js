b236968abaee3e882a39ddd10d08959e1e766946
Connie Liu <heyconnie123@gmail.com>
2017-10-09 17:18:19 -0400

    filter for station name in fetch, express mongodb backend added with sample schema wip

--- NYSee/src/components/NearestStations.js
+++ NYSee/src/components/NearestStations.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Picker } from 'react-native';
 
 export default class NearestStations extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      stationData: ""
+    }
+  }
   render() {
+    fetch('https://data.ny.gov/resource/hvwh-qtfg.json?$where=within_circle(entrance_location%2C40.824261%2C-73.945537%2C150)')
+    .then(response =>
+        response.json()
+        .then(data => ({
+            data: data,
+            status: response.status
+        })
+    ).then(res => {
+        // this.setState({stationData: res.data});
+        for(var i in res.data){
+          var key = i;
+          var val = res.data[i];
+          for(var j in val){
+            var sub_key = j;
+            if(sub_key == "station_name"){
+              this.setState({stationData: String(val[j])});
+            }
+          }
+        }
+    }))
     return (
       <View style={styles.container}>
         <Text>NEAREST STATIONS WIP</Text>
         <Text>....................</Text>
-        <Text>Footer Will Also Change As We Go!</Text>
+        <Text>{this.state.stationData}</Text>
       </View>
     );
   }
